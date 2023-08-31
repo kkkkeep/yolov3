d6b5bfb63e3175378400544568bc0700360e225e
kkkkeep <2532872658@qq.com>
2023-08-31 13:46:00 +0800

    aliyun dataprepare

--- data_prepare.py
+++ data_prepare.py
@@ -7,7 +7,7 @@ CLASS = ['person', 'bird', 'cat', 'cow', 'dog', 'horse', 'sheep',
 
 def data_process(anno_path):
     xml_list = os.listdir(anno_path)
-    with open("/mnt/data/pascal_voc_2007_train/label.txt", 'w') as f:
+    with open("/mnt/data/voc_2007/label.txt", 'w') as f:
         for xml_file_name in tqdm(xml_list):
             data = []
             xml_file = os.path.join(anno_path, xml_file_name)
@@ -30,7 +30,7 @@ def data_process(anno_path):
             data = [str(i) for i in data]
             f.write(' '.join(data) + '\n')
 if __name__ == '__main__':
-    annotation_path = "/mnt/data/pascal_voc_2007_train/Annotations/"
+    annotation_path = "/mnt/data/voc_2007/Annotations/"
 
     data_process(annotation_path)
     print(len(os.listdir(annotation_path)))
